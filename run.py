262fe831942e4d0745ee75bf57464b671f49ca11
davis <davisochieng4@gmail.com>
2019-02-08 11:12:30 +0300

    Update failing heroku run.py file

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ from app import create_app
 from instance import config
 
 
-if __name__ == '__main__':
-    app = create_app(config.DevelopmentConfig)
-    app.run(debug=True)
+
+app = create_app(config.DevelopmentConfig)
+app.run(debug=True)
