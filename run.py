79708805e5c6c832c805255522dc73bb80fa1c7d
davis <davisochieng4@gmail.com>
2019-02-08 13:52:20 +0300

    Add default route

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
 """A  module to run the application"""
 from app import create_app
-# from instance.config import app_config
+from flask import jsonify
 from instance import config
 app = create_app(config.DevelopmentConfig)
 
+@app.route("/"):
+def home():
+    response = dict{
+        message = "Welcome Politico, go to /api/v1/parties to see all the parties"
+    }
+    return jsonify(response)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
