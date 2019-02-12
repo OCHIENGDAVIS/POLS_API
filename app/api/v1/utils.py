8cdb56f97f1c3f295c520d077a65966322bf6d0b
davis <davisochieng4@gmail.com>
2019-02-12 19:26:18 +0300

    Refactor tests

--- app/api/v1/utils.py
+++ app/api/v1/utils.py
@@ -1,77 +1,86 @@
 from app.api.v1.models.party_models import parties
-
-# class DataError(Exception):
-#     pass
-
-# def validate_party_info(party_dict):
-#     """A utility function to validate party inputs"""
-#     data = party_dict
-#     id = data.get("id", None)
-#     name = data.get('name', None)
-#     hqAddress = data.get("hqAddress", None)
-#     logoUrl = data.get("logoUrl", None)
-#     if id is None:
-#         return {"message": "id is required", "code": 400}
-#     elif not isinstance(id, int):
-#         return {"message": "id must be a number", "code": 400}
-#     elif name is None:
-#         return {"message": "name is required", "code": 400}
-#     elif len(name) < 1:
-#         return {"message": "name can not be an empty string", "code": 400}
-#     elif not isinstance(name, str):
-#         return {"message": "name must be a string", "code": 400}
-#     elif hqAddress is None:
-#         return {"message": "hqAddress is required", "code": 400}
-#     elif len(hqAddress) < 1:
-#         return {"message": "hqaddress  can not be an empty string", "code": 400}
-#     elif logoUrl is None:
-#         return {"message": "party logo Url is required", "code": 400}
-#     elif len(logoUrl) < 1:
-#         return {"message": "Logo url  can not be an empty string", "code": 400}
-#     elif not isinstance(logoUrl, str):
-#         return {"message": "logo must be a string", "code": 400}
-#     elif next(filter(lambda x: x['id'] == id, parties), None):
-#         return {"message": "party with that id already exists", "code": 400}
-
-
-# def find_by_id(item_id, item_iterable ):
-#     if next(filter(lambda x:x['id']== item_id, item_iterable), None):
-#         return True
-#     else:
-#         return False
-def validate_keys(data, fields):
+from app.api.v1.models.office_models import offices
+def check_json(data):
     if not data:
         return False
-    if not fields:
-        return False
-    for key, _ in fields.items():
-        if data.get(key) is None and not key in list(data.keys()):
-            return False
     return True
 
+class ValidateParty():
+    def __init__(self, id, name, hqAddress, logoUrl):
+        self.id = id
+        self.name = name
+        self.hqAddress = hqAddress
+        self.logoUrl = logoUrl
 
-def validate_value_types(data, fields):
-    if not data:
-        return False
-    if not fields:
-        return False
-    for key, value in data.items():
-        if type(value) == fields.get(key):
-            return True
-    return False
-
-def find_by_id(_id):
-    """A utility funtion to find a particular party by id"""
-    for item in parties:
-        if item['id'] == _id:
-            return True
-    return False
-
-
-def find_item_by_id(item_id, item_iterable):
-    """A utility function to find a particular party by name"""
-    if next(filter(lambda x: x['id'] == item_id, item_iterable), None):
+    def check_id(self):
+        if not isinstance(self.id, int):
+            return False
         return True
-    else:
+
+    def check_name(self):
+        if not isinstance(self.name, str)  or  (self.name==""):
+            return False
+        return True
+
+    def check_hqaddress(self):
+        if not isinstance(self.hqAddress, str) or (self.hqAddress == ""):
+            return False
+        return True
+
+    def check_logoUrl(self):
+        if not isinstance(self.logoUrl, str) or (self.logoUrl== ""):
+            return False
+        return True
+
+    @classmethod
+    def party_exists(cls, id):
+        for party in parties:
+            if party["id"] == id:
+                return True
         return False
+
+    @classmethod
+    def get_party_by_id(cls, id):
+        for party in parties:
+            if party["id"] == id:
+                return party
+        return None
+
+
+class ValidateOffice():
+    def __init__(self, id, name, type):
+        self.id = id
+        self.name = name
+        self.type = type
+
+    def check_office_id(self):
+        if not isinstance(self.id, int):
+            return False
+        return True
+
+    def check_office_name(self):
+        if not isinstance(self.name, str) or (self.name == ""):
+            return False
+        return True
+
+    def check_office_type(self):
+        if not isinstance(self.type, str) or (self.type == ""):
+            return False
+        return True
+
+    @classmethod
+    def office_exists(cls, id):
+        for office in  offices:
+            if office["id"] == id:
+                return True
+        return False
+
+    @classmethod
+    def get_office_by_id(cls, id):
+        for office in offices:
+            if office["id"] == id:
+                return office
+        return None
+
+
     
--- app/api/v1/views/routes.py
+++ app/api/v1/views/routes.py
@@ -2,7 +2,7 @@ from flask import jsonify, request
 from flask import Blueprint
 from app.api.v1.models.party_models import Party, parties
 from app.api.v1.models.office_models import Office, offices
-from app.api.v1.utils import find_item_by_id, validate_keys, validate_value_types
+from app.api.v1.utils import ValidateOffice, ValidateParty, check_json
 
 api = Blueprint('api', __name__)
 
@@ -12,7 +12,7 @@ def get_all_parties():
     data = []
     for party in parties:
         temp_party = {
-            # "id": party["id"],
+            "id": party["id"],
             "name": party["name"],
             "hqAddress": party["hqAddress"],
             "logoUrl": party["logoUrl"]
@@ -27,31 +27,24 @@ def get_all_parties():
 @api.route('/parties', methods=['POST'])
 def create_a_party():
     data = request.get_json(force=True)
-    allowed_fields = {
-        "id": int,
-        "name": str,
-        "hqAddress": str,
-        "logoUrl": str
-    }
-    keys_validation_response = validate_keys(data,allowed_fields )
-    valuetypes_validation_response = validate_keys(data, allowed_fields)
-    if next(filter(lambda x: x['id'] == data['id'], parties), None):
-        return jsonify({"message": "party with that id already exists", "code": 400}),400
-    elif keys_validation_response and valuetypes_validation_response:
-        new_party = Party.create_party(
-            data['id'], data['name'], data['hqAddress'], data['logoUrl'])
+    party = ValidateParty(data.get('id'), data.get('name'), data.get("hqAddress"), data.get("logoUrl"))
+    if check_json(data) and party.check_name() and party.check_id() and party.check_hqaddress() and party.check_logoUrl() and  not(ValidateParty.party_exists(party.id)):
+        new_party = Party.create_party(party.id, party.name, party.hqAddress, party.logoUrl)
         return jsonify({
             "status": 201,
-            "data": [
-                {
-                    "id": new_party["id"],
-                    "name": new_party["name"]
-                },
-            ]
+            "data": [{
+                "id": new_party["id"],
+                "name": new_party["name"]
+            }]
         }), 201
-    else:
-        return jsonify({"message": "Data input error please check the data you are trying to submit"}), 400
+    return jsonify({
+        "status": 400,
+        "data": [{
+            "message": "Error please check the data you are trying to submit"
+        }]
+    }), 400
 
+    
 
 @api.route('/parties/<int:party_id>', methods=['GET'])
 def get_a_party(party_id):
@@ -63,48 +56,42 @@ def get_a_party(party_id):
 
 @api.route('/parties/<int:party_id>', methods=['DELETE'])
 def delete_a_party(party_id):
-        for party in parties:
-            if party['id'] == party_id:
-                party_index = parties.index(party)
-                del(parties[party_index])
-                return jsonify({
-                    "status": 200,
-                    "data": [
-                        {
-                            "message": "party deleted successfully"
-                        }
-                    ]
-                }), 200
+    party = ValidateParty.get_party_by_id(party_id)
+    if  party:
+        party_index = parties.index(party)
+        del(parties[party_index])
         return jsonify({
-            "status": 404,
-            "data": [{
-                "message": "party does not exists"
-            }]
-        }), 404
+            "status": 200,
+            "data": [
+                {
+                    "message": "party deleted successfully"
+                }
+            ]
+        }), 200
+    return jsonify({
+        "message": "party does not exists"
+    })
+        
 
 
 @api.route('/parties/<int:party_id>/name', methods=['PATCH'])
 def edit_a_party(party_id):
     data = request.get_json()
-    name = data.get("name", None)
-    if name is None:
-        return jsonify({'message': 'You must provide the new  name'}), 400
-    elif len(data['name']) < 0:
-        return jsonify({"message": "name cannot be empty"})
-    else:
-        for party in parties:
-            if party['id'] == party_id:
-                party['name'] = data['name']
-                return jsonify({
+    if check_json(data) and data.get("name") is not None:
+        party_to_edit = ValidateParty.get_party_by_id(party_id)
+        if party_to_edit:
+            party_to_edit['name'] = data['name']
+            return jsonify({
                     "status": 200,
                     "data": [
                         {
-                            "id": party['id'],
-                            "name": party['name']
+                            "id": party_to_edit['id'],
+                            "name": party_to_edit['name']
                         }
                     ]
                 })
-        return jsonify({"message": "Party does not exists"}), 404
+    return jsonify({"message": "Party does not exists"}), 404
+   
 
 
 @api.route('/offices', methods=['GET'])
@@ -124,27 +111,27 @@ def get_all_offices():
 
 
 @api.route('/offices', methods=['POST'])
-def create_an_office():
-    data = request.get_json(force=True)
-    allowed_fields = {
-        "id": int,
-        "name": str,
-        "type": str,
-    }
-    keys_validation_response = validate_keys(data, allowed_fields)
-    valuetypes_validation_response = validate_keys(data, allowed_fields)
-    if next(filter(lambda x: x['id'] == data['id'], offices), None):
-        return jsonify({"message": "Office with that id already exists", "code": 400}), 400
-    if keys_validation_response and valuetypes_validation_response:
-        new_office = Office.create_office(data['id'], data['type'], data['name'])
-        return jsonify({
-            "status": 201,
-            "data": [{
-                "id": new_office["id"],
-                "type": new_office["type"],
-                "name": new_office["name"]
-            }]
-        }), 201
+# def create_an_office():
+#     data = request.get_json(force=True)
+#     allowed_fields = {
+#         "id": int,
+#         "name": str,
+#         "type": str,
+#     }
+#     keys_validation_response = validate_keys(data, allowed_fields)
+#     valuetypes_validation_response = validate_keys(data, allowed_fields)
+#     if next(filter(lambda x: x['id'] == data['id'], offices), None):
+#         return jsonify({"message": "Office with that id already exists", "code": 400}), 400
+#     if keys_validation_response and valuetypes_validation_response:
+#         new_office = Office.create_office(data['id'], data['type'], data['name'])
+#         return jsonify({
+#             "status": 201,
+#             "data": [{
+#                 "id": new_office["id"],
+#                 "type": new_office["type"],
+#                 "name": new_office["name"]
+#             }]
+#         }), 201
 
 
 @api.route('/offices/<int:office_id>', methods=['GET'])
